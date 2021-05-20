05a360b5a99d54fc468d2e0119662d7a5aa80217
Vitor Garcia <vitor.stabile@hotmail.com>
2021-05-20 16:24:52 +0100

    Feign Client

--- hr-payroll/src/main/java/com/vitorgarcia/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/vitorgarcia/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,9 @@ package com.vitorgarcia.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
